b36ebc522a4ecc5e2f440a0a56db863c0b5f2a55
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-31 18:07:35 -0300

    Recebendo a mensagem do frontend. Note no terminal que ao carregar o navegador aparece a mensagem:{usuario: 'Rodrigo'}

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -3,5 +3,8 @@ const { io } = require('../server');
 
 io.on('connection', (client) => {
 
-    
+    //Configurando o listener para receber a mensagem do frontend
+    client.on('entrarChat', (usuario) =>{
+        console.log(usuario);
+    })
 });
