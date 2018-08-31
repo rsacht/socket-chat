a1114837f232ceaf0acdf2a26857fb549f4426f8
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-31 18:30:56 -0300

    Pegando o nome pela URL. Note no terminal que ao carregar o navegador com a url: http://localhost:3000/chat.html?nome=Rodrigo o retorno: { usuario: { nome: 'Rodrigo' } }

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,12 +1,24 @@
 var socket = io();
 
+//Configurando uma variável para o nome do usuário
+var params = new URLSearchParams(window.location.search);
+//Se não vier o nome do usuário na URL dispara um erro
+if(!params.has('nome')){
+    window.location = 'index.html';
+    throw new Error('O nome é necessário');
+}
+
+var usuario = {
+    nome: params.get('nome')
+};
+
 socket.on('connect', function() {
     console.log('Conectado ao servidor');
 
     //Quando a pessoa se conecta pelo frontend
     //Vamos mandar uma mensagem para o servidor informando 
     //Quem é a pessoa que está entrando no chat
-    socket.emit('entrarChat', {usuario: 'Rodrigo'});
+    socket.emit('entrarChat', {usuario});
 });
 
 // escuchar
