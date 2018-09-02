1d03649463c8fd1a84f02be83f27a14f566a57ea
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-02 18:40:46 -0300

    Renderizando o nome da sala

---
+++ public/js/socket-chat-jquery.js
@@ -0,0 +1,8 @@
+//Funções para renderizar usuários
+function renderizarUsuarios(pessoas){//Array esperado [{},{},{}]
+    console.log(pessoas);
+    var html = '';
+    html += '<li>';
+    html += '    <a href="javascript:void(0)" class="active"> Chat de <span> Juegos</span></a>'
+    html += '</li>';
+}
