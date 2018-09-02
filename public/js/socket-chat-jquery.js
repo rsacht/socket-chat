9b2dbb04be251c093450d32f74f034af4429c6f9
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-02 18:45:09 -0300

    Renomeando o nome da sala pela palavra que vem na URL

--- public/js/socket-chat-jquery.js
+++ public/js/socket-chat-jquery.js
@@ -1,8 +1,11 @@
+var params = new URLSearchParams(window.location.search);
+
 //Funções para renderizar usuários
 function renderizarUsuarios(pessoas){//Array esperado [{},{},{}]
     console.log(pessoas);
     var html = '';
+
     html += '<li>';
-    html += '    <a href="javascript:void(0)" class="active"> Chat de <span> Juegos</span></a>'
+    html += '    <a href="javascript:void(0)" class="active"> Chat de <span> '+ params.get('sala') +'</span></a>'
     html += '</li>';
 }
